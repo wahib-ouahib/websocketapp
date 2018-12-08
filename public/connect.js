06f280f8f6228e1f76c8d58f929a25db2b39cdba
wahib <wahib.med.ouahib@gmail.com>
2018-12-08 22:10:03 +0000

    adding event listener on click & rendring the data as an output

--- public/connect.js
+++ public/connect.js
@@ -1,1 +1,24 @@
-var socket = io.connect('http://localhost:2019')
+//connect front to socket.io
+var socket = io.connect('http://localhost:2019');
+
+//Query DOM
+var message = document.getElementById("message");
+var handle  = document.getElementById("handle");
+var output  = document.getElementById("output");
+var btn    = document.getElementById("send");
+
+//Emit events
+btn.addEventListener('click', function(){
+    socket.emit('chat', data = {
+        handle: handle.value,
+        message: message.value
+    })
+    console.log(message);
+    message.value = "";
+})
+
+//Listen for events
+socket.on('chat', function(data){
+    console.log(data.message)
+    output.innerHTML += `<p><strong>${data.handle} : </strong>${data.message}</p>`
+})

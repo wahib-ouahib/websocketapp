a873067aded52fd0c12170cd25f46aa04e3ac332
wahib <wahib.med.ouahib@gmail.com>
2018-12-08 10:43:07 +0000

    setting up the server and websockets

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-const mongo = require('mongodb').MongoClient;
-const io = require('socket.io').listen(4000).sockets;
 
+var express = require('express')
 
-//-------------------------connect to Mongo
-
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Use connect method to connect to the server
-mongo.connect(url, function(err, client) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-  
-    const db = client.db(dbName);
-  
-    client.close();
-  });

a46ac47d0c375349c50c7a0302ecf609109879c2
wahib <wahib.med.ouahib@gmail.com>
2018-12-08 23:12:56 +0000

    add typing event listener

--- public/connect.js
+++ public/connect.js
@@ -2,10 +2,11 @@
 var socket = io.connect('http://localhost:2019');
 
 //Query DOM
-var message = document.getElementById("message");
-var handle  = document.getElementById("handle");
-var output  = document.getElementById("output");
-var btn    = document.getElementById("send");
+var message  = document.getElementById("message");
+var handle   = document.getElementById("handle");
+var output   = document.getElementById("output");
+var btn      = document.getElementById("send");
+var feedback = document.getElementById("feedback");
 
 //Emit events
 btn.addEventListener('click', function(){
@@ -17,8 +18,17 @@ btn.addEventListener('click', function(){
     message.value = "";
 })
 
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
+})
+
+
 //Listen for events
 socket.on('chat', function(data){
-    console.log(data.message)
+    feedback.innerHTML = '';
     output.innerHTML += `<p><strong>${data.handle} : </strong>${data.message}</p>`
 })
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});

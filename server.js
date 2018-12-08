c8b077dff92ea6e1ce96e8557d69e40806bac6c3
wahib <wahib.med.ouahib@gmail.com>
2018-12-08 16:19:33 +0000

    rendering static files

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+var express = require('express');
 
-var express = require('express')
+var app = express();
 
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.text()); 
+//app.use(bodyParser.json()); 
+
+port = 2019 || process.env.PORT;
+
+//listening to port
+app.listen(port, ()=>console.log(`we're live  at port ${port}`));

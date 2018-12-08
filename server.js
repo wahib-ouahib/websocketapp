8726ba199c296b14d6be47347d96ecfbba2b7869
wahib <wahib.med.ouahib@gmail.com>
2018-12-08 19:56:20 +0000

    setting the socket for the user

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-var express = require('express');
-
-var app = express();
-
-//app.use(bodyParser.urlencoded({extended: true}));
-//app.use(bodyParser.text()); 
-//app.use(bodyParser.json()); 
+const express = require('express');
+const socket  = require('socket.io');
+const app = express();
 
 port = 2019 || process.env.PORT;
 
-//listening to port
-app.listen(port, ()=>console.log(`we're live  at port ${port}`));
+    //listening to port
+var server = app.listen(port, ()=>{console.log(`we're live at port ${port}`)});
+
+    //Static files
+app.use(express.static('./public'));
+
+    //Socket setup
+const io = socket(server);
+
+io.on('connection',(socket)=>console.log('socket connection established'))
